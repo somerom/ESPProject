38bbc79960126887e4e2a634c64f68a37f94704c
Johanna Nygård <johanna.nygard@metropolia.fi>
2020-02-28 12:38:50 +0200

    remove test

--- lpc_chip_15xx/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- C:\Users\johan\Documents\MCUXpressoIDE_11.1.0_3209\project\lpc_chip_15xx\inc/ring_buffer.h \
- C:\Users\johan\Documents\MCUXpressoIDE_11.1.0_3209\project\lpc_chip_15xx\inc/lpc_types.h
-
-C:\Users\johan\Documents\MCUXpressoIDE_11.1.0_3209\project\lpc_chip_15xx\inc/ring_buffer.h:
-
-C:\Users\johan\Documents\MCUXpressoIDE_11.1.0_3209\project\lpc_chip_15xx\inc/lpc_types.h:
